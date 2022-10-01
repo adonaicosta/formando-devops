c60b07aaf9639a3cb342e621f4ef13f497a7a8fe
Guilherme Dias <guilhermerrdias@gmail.com>
2022-10-01 06:17:57 -0300

    atualizacao: condicional para regiao do workspace

--- aulas-terraform/main.tf
+++ aulas-terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-	region = "us-east-1"
+	region = "${terraform.workspace == "production" ? "us-east-1" : "us-east-2"}"
 	version = "~> 2.0"
 }
 
@@ -8,5 +8,6 @@ terraform {
 		bucket = "descomplicando-terraform-guilherme-tfstates" /* nome apenas de exemplo */
 		key    = "terraform-test.tfstate"
 		region = "us-east-1"
+		encrypt = true
 	}
 }
