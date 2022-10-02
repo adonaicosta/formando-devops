e700c3aa911151236bfab941df448695fa551bc0
Guilherme Dias <80716667+Siluryan@users.noreply.github.com>
2022-10-02 11:59:53 -0300

    Update terrafile.tf

--- desafio-terraform/1/terrafile.tf
+++ desafio-terraform/1/terrafile.tf
@@ -1,6 +1,6 @@
 module "clusters" {
-	source = "./clusters"
-    cluster_name = "ClusterFormandoDevOps1"
+    source             = "./clusters"
+    cluster_name       = "ClusterFormandoDevOps1"
     kubernetes_version = "kindest/node:v1.18.4"
 }
 
