7f21c7411f843faf7d93f639e732f8914ab77b7f
Guilherme Dias <80716667+Siluryan@users.noreply.github.com>
2022-10-03 22:40:32 -0300

    Update locals.tf

--- desafio-terraform/3/locals.tf
+++ desafio-terraform/3/locals.tf
@@ -1,8 +1,8 @@
 locals {  
     iterator = {          
       for i in range(100) :
-        div = i        
-        condition (100%div == 0 ? div_list[div_index] = div &&
+        index = i        
+        condition (index%div == 0 ? div_list[div_index] = div &&
         div_index = div_index + 1:)       
       }
 }
