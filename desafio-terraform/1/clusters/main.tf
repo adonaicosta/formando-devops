26df9aa09a1634a3394583f229f65ef4fad8a98b
Guilherme Dias <80716667+Siluryan@users.noreply.github.com>
2022-10-05 08:29:08 -0300

    Update main.tf

--- desafio-terraform/1/clusters/main.tf
+++ desafio-terraform/1/clusters/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kind =  {
+    source = "kyma-incubator/kind"
+    version = "0.0.11"
+    }
+  }
+}
+
 resource "kind_cluster" "default" {
     name = var.cluster_name
     node_image = var.kubernetes_version      
@@ -7,15 +16,8 @@ resource "kind_cluster" "default" {
         api_version = "kind.x-k8s.io/v1alpha4"              
 
         node {
-            role = "infra"           
-            taints = [
-                {
-                  key    = "dedicated"
-                  value  = "statefulset"
-                  effect = "NO_SCHEDULE"
-                }
-            ] 
-        }           
+            role = "infra"      
+              
                     
         node {
             role =  "app"
